ee18d31a2ed32383e2a55bfdd7f597dd68a109a7
rohxn08 <rohanramaswamy@gmail.com>
2025-11-24 13:50:07 +0530

    Added GridSearchCV but still overfitting (TO ADD DIMENSIONALITY REDUCTION)

--- src/models/train_svm.py
+++ src/models/train_svm.py
@@ -7,6 +7,7 @@ import os
 from src.features.lbp_extractor import LBPExtractor
 import cv2
 from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import GridSearchCV
 
 
 dir=r"data\MSU-MFSD\pics"
@@ -38,10 +39,16 @@ def train_model(x,y):
     scaler=StandardScaler()
     xsca=scaler.fit_transform(x)
     xtr,xte,ytr,yte=train_test_split(xsca,y,test_size=0.2,random_state=42)
-    model=svm.SVC(kernel='rbf',class_weight='balanced')
-    model.fit(xtr,ytr)
+    xte=scaler.fit_transform(xte)
+    param_grid={"C":[0.1,1,10,100],
+                "gamma":["scale","auto",0.01,0.001],
+                "kernel":["rbf"]}
+    grid=GridSearchCV(estimator=SVC(class_weight="balanced"),param_grid=param_grid,scoring="f1",cv=3,n_jobs=-1,verbose=1)
+    grid.fit(xtr,ytr)
+    model=grid.best_estimator_
     
-    return model,xte,yte
+    
+    return model,scaler,xte,yte
     
     
 def predict(model,xte,yte):
@@ -51,7 +58,7 @@ def predict(model,xte,yte):
 
 if __name__=="__main__":
     x, y = preprocessing(real_dir, attack_dir)
-    model, xte, yte = train_model(x, y)
+    model,scaler, xte, yte = train_model(x, y)
     y_preds = predict(model, xte, yte)
     print("Accuracy:", accuracy_score(yte, y_preds))
     print("Precision:", precision_score(yte, y_preds))
